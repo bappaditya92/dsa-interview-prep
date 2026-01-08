dddd4198e5f2dcc56bbc4d62ebbe6811b28bdf44
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-08 19:29:01 +0530

    remove some extra space

--- maxProdOfSplittedBinTree.js
+++ maxProdOfSplittedBinTree.js
@@ -1,22 +1,21 @@
-const maxProduct = (root)=> {
-    const MOD = 1000000007n;
-    let totalSum = 0n;
-    let maxProd = 0n;
-    const getTotalSum = (node)=> {
-        if (!node) return 0n;
-        return BigInt(node.val) + getTotalSum(node.left) + getTotalSum(node.right);
-    }
-    totalSum = getTotalSum(root);
-    const dfs= (node)=> {
-        if (!node) return 0n;
-        const leftSum = dfs(node.left);
-        const rightSum = dfs(node.right);
-        const subTreeSum = BigInt(node.val) + leftSum + rightSum;
-        const product = subTreeSum * (totalSum - subTreeSum);
-        maxProd = maxProd > product ? maxProd : product;
-        return subTreeSum;
-    }
-    dfs(root);
-
-    return Number(maxProd % MOD);
+const maxProduct = (root) => {
+  const MOD = 1000000007n;
+  let totalSum = 0n;
+  let maxProd = 0n;
+  const getTotalSum = (node) => {
+    if (!node) return 0n;
+    return BigInt(node.val) + getTotalSum(node.left) + getTotalSum(node.right);
+  };
+  totalSum = getTotalSum(root);
+  const dfs = (node) => {
+    if (!node) return 0n;
+    const leftSum = dfs(node.left);
+    const rightSum = dfs(node.right);
+    const subTreeSum = BigInt(node.val) + leftSum + rightSum;
+    const product = subTreeSum * (totalSum - subTreeSum);
+    maxProd = maxProd > product ? maxProd : product;
+    return subTreeSum;
+  };
+  dfs(root);
+  return Number(maxProd % MOD);
 };
