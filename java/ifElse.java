4765e0e3da2dbf4eba6363633d97e35060fce2ae
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-11 22:18:05 +0530

    Add Ifelse class with conditional statements

---
+++ java/ifElse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b)
+            System.out.println("A is greater");
+        else if (a == 10)
+            System.out.println("A is ten");
+        else
+            System.out.println("None");
+    }
+}
