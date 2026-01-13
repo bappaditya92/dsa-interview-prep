81a2929f2e7fca1e7e929be911a2d5220998c370
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-13 21:57:08 +0530

    Implement logic to find the second largest number

---
+++ java/arr.java
@@ -0,0 +1,23 @@
+class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest element not found");
+        } else {
+            System.out.println("Second largest element is: " + secondLargest);
+        }
+    }
+}
