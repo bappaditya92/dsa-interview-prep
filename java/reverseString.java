0114b38ab4c7e3d4a0c7b9c028808166a502050a
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-16 23:22:40 +0530

    Implement string reversal logic in ReverseString class

--- java/reverseString.java
+++ java/reverseString.java
@@ -2,7 +2,6 @@ public class ReverseString {
     public static void main(String[] args) {
         String s = "hello";
         String rev = "";
-
         for (int i = s.length() - 1; i >= 0; i--) {
             rev += s.charAt(i);
         }
