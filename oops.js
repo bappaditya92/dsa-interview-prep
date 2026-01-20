9f4f9a67295164a097de85f2bba0e054bb49edfa
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-20 18:48:31 +0530

    Refactor LRUCache constructor for cleaner code
    Removed unnecessary line break in LRUCache constructor.

---
+++ oops.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+    
+    this.head = new Node(null, null);
+    this.tail = new Node(null, null);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  _remove(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+
+  _add(node) {
+    node.next = this.head.next;
+    node.prev = this.head;
+    this.head.next.prev = node;
+    this.head.next = node;
+  }
+
+  get(key) {
+    if (!this.map.has(key)) return -1;
+
+    const node = this.map.get(key);
+    this._remove(node);
+    this._add(node);
+    return node.value;
+  }
+
+  put(key, value) {
+    if (this.map.has(key)) {
+      this._remove(this.map.get(key));
+    }
+
+    const node = new Node
