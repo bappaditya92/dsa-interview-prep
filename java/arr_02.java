d728190927dc44d8a67c3eb4a034f359e6cfb4dd
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-21 11:11:30 +0530

    replace code destination

--- java/arr_02.java
+++ java/arr_02.java
@@ -1,15 +1,12 @@
 class LargestElement {
     public static void main(String[] args) {
         int[] arr = {3, 7, 2, 9, 4};
-
         int max = arr[0];
-
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
         }
-
         System.out.println("Largest element: " + max);
     }
 }
