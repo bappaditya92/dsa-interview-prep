2fd4bb7b27ff27e03b2b78ae0b53ef4ed5cd736c
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-23 18:31:57 +0530

    Convert findMissing to an arrow function

--- missingNumber.js
+++ missingNumber.js
@@ -1,7 +1,7 @@
-function findMissing(arr, n) {
+const findMissing = (arr, n) => {
   const total = (n * (n + 1)) / 2;
   const sum = arr.reduce((a, b) => a + b, 0);
   return total - sum;
 }
 
-console.log(findMissing([1, 2, 4, 5], 5)); // 3
+console.log(findMissing([1, 2, 4, 5], 5));
