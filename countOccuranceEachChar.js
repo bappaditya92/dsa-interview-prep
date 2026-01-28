7eb77dddde79167397bdc1e396340b7b2973825b
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-28 21:42:51 +0530

    remove a code and move a java code to folder

--- countOccuranceEachChar.js
+++
@@ -1,8 +0,0 @@
-const countCharacters = (str) => {
-  return str.split("").reduce((acc, char) => {
-    acc[char] = (acc[char] || 0) + 1;
-    return acc;
-  }, {});
-}
-
-console.log(countCharacters("hello"));
