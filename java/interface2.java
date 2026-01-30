941c836af1264cc9b1537da8de1a2fdc35e1022b
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-30 22:03:57 +0530

    Create interface2.java

---
+++ java/interface2.java
@@ -0,0 +1,12 @@
+interface Animal {
+    void sound();  
+}
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Bark");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Dog();   
+        a
