a66c0c260a1bd52648bc7415b5ffe8524d74c2ac
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-31 22:32:58 +0530

    Create topThreeNum.java

---
+++ java/topThreeNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Top3Numbers {
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(10, 50, 30, 90, 70, 20);
+
+        nums.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .forEach(System.out::println);
+    }
+}
