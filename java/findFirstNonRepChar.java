81fe59025134a90477bc2445609dd0b7ad7ee087
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-01-31 22:23:10 +0530

    Create find First Non Repeatative Character

---
+++ java/findFirstNonRepChar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeat {
+    public static void main(String[] args) {
+
+        String input = "aabbcddeff";
+
+        Character result = input.chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+
+        System.out.println(result);
+    }
+}
