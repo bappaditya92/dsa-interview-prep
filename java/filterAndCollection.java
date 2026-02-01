1971f203183c89ee108ee071eab59b4ac0d5ed1a
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-02-01 21:37:58 +0530

    Create filterAndCollection code

---
+++ java/filterAndCollection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6);
+
+        List<Integer> evens = nums.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(evens);
+    }
+}
