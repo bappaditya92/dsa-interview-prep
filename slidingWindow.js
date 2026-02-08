4e34d0c165da4a9c16b9f18fa1fcb891250d7935
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-02-08 21:32:20 +0530

    Create slidingWindow leetcode problem

---
+++ slidingWindow.js
@@ -0,0 +1,16 @@
+const lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0, max = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1);
+    }
+
+    return max;
+};
