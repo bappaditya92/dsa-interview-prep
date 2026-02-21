ad8461f73eaf1f5d0aa8d8ad6157a5e03bcd0519
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-02-21 23:05:11 +0530

    Implement reverseList function to reverse a linked list

---
+++ reverseList.js
@@ -0,0 +1,14 @@
+const reverseList = (head) => {
+
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
