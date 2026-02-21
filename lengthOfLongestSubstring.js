30b57afc4671e5e7c2893b18789f51c0b01684d8
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-02-21 23:00:14 +0530

    Implement function to find longest substring length

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,18 @@
+const lengthOfLongestSubstring = (s) => {
+  let set = new Set();
+  let left = 0;
+  let max = 0;
+
+  for (let right = 0; right < s.length; right++) {
+
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(s[right]);
+    max = Math.max(max, right - left + 1);
+  }
+
+  return max;
+};
