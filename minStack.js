784ac3887ce16d00e624cf8c73aed18828fcb176
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-02-22 22:20:29 +0530

    Implement MinStack class with basic operations

---
+++ minStack.js
@@ -0,0 +1,39 @@
+class MinStack {
+
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val) {
+
+    this.stack.push(val);
+
+    if (
+      this.minStack.length === 0 ||
+      val <= this.getMin()
+    ) {
+      this.minStack.push(val);
+    }
+
+  }
+
+  pop() {
+
+    const removed = this.stack.pop();
+
+    if (removed === this.getMin()) {
+      this.minStack.pop();
+    }
+
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+    return this.minStack[this.minStack.length - 1];
+  }
+
+}
