c008fdc68b5a28c8dfee979f6ce734d8ac0ed1e1
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-03-02 23:06:34 +0530

    Add Top 3 Highest Paid Employees code

---
+++ Stream3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Employee {
+
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    double getSalary() {
+        return salary;
+    }
+
+    String getName() {
+        return name;
+    }
+}
+
+public class Top3HighestPaid {
+
+    public static void main(String[] args) {
+
+        List<Employee> list = Arrays.asList(
+                new Employee(1, "Rahul", 50000),
+                new Employee(2, "Amit", 90000),
+                new Employee(3, "Neha", 70000),
+                new Employee(4, "Priya", 120000),
+                new Employee(5, "Vikash", 110000)
+        );
+
+        List<Employee> top3 = list.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+
+        for (Employee e : top3) {
+            System.out.println(e.getName() + " : " + e.getSalary());
+        }
+    }
+}
