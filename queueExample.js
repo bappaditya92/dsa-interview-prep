4ebba9c6f7625c6cda6fbf6995cfb18bf2415379
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-03-21 23:24:02 +0530

    Implement Queue Using Stacks

---
+++ queueExample.js
@@ -0,0 +1,34 @@
+class QueueStack{
+    constructor(){
+        this.stack1 = []
+        this.stack2 = []
+    }
+
+    push(x){
+        while(this.stack1.length > 0){
+            this.stack2.push(this.stack1.pop())
+        }
+
+        this.stack1.push(x);
+
+        while(this.stack2.length > 0){
+            this.stack1.push(this.stack2.pop())
+        }
+    };
+
+    pop(){
+        if(this.empty()){
+            return null;
+        }
+
+        return this.stack1.pop()
+    };
+
+    peek(){
+        return this.empty() ? null : this.stack1.at(-1)
+    };
+
+    empty(){
+        return this.stack1.length === 0
+    };
+}
