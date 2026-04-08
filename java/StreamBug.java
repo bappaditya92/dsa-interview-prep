e19d5984971e4ed9ff651db3cc177833d80b06c3
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-04-08 23:38:16 +0530

    Add StreamBug class to illustrate mutable state issue
    Demonstrates a bug with mutable state in stream reduce.

---
+++ java/StreamBug.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class StreamBug {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        List<Integer> result = list.stream().reduce(
+                new ArrayList<>(),
+                (acc, item) -> {
+                    acc.add(item);
+                    return acc; // BAD (shared mutable state)
+                },
+                (left, right) -> {
+                    left.addAll(right);
+                    return left;
+                }
+        );
+
+        System.out.println(result);
+    }
+}
