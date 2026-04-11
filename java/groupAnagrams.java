2c25b47fc72ba5a0e74f3efaa47dab2815f4c80c
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-04-12 00:17:37 +0530

    Implement groupAnagrams function in Java

---
+++ java/groupAnagrams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        System.out.println(map.values());
+    }
+}
