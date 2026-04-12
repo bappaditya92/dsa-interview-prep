4cfb4d62c10c7a3cc69d785f62ba55600a6b8fa5
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-04-12 22:55:51 +0530

    Add CompareLists class to compare two lists

---
+++ java/CompareLists.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CompareLists {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+
+        System.out.println(list1.equals(list2)); // true
+    }
+}
