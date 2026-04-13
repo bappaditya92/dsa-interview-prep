0926b02b78455d1a4b08e85d81413c76d877f835
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-04-13 22:13:38 +0530

    Implement Sliding Window Maximum algorithm

---
+++ SlidingWindowMax.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SlidingWindowMax {
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
+                dq.poll();
+            }
+
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offer(i);
+
+            if (i >= k - 1) {
+                result.add(nums[dq.peek()]);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
