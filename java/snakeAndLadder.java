397fcc8906c78b06c4dd676e1db1647129a9fcf9
bappaditya92 <bappadityasinha.92@gmail.com>
2026-04-24 11:20:23 +0530

    refractor codebase

--- java/snakeAndLadder.java
+++ java/snakeAndLadder.java
@@ -1,73 +1,73 @@
-import java.util.*;
+// import java.util.*;
 
-class Player {
-    String name;
-    int position;
+// class Player {
+//     String name;
+//     int position;
 
-    Player(String name) {
-        this.name = name;
-        this.position = 0;
-    }
-}
+//     Player(String name) {
+//         this.name = name;
+//         this.position = 0;
+//     }
+// }
 
-class SnakeLadder {
-    private Map<Integer, Integer> snakes = new HashMap<>();
-    private Map<Integer, Integer> ladders = new HashMap<>();
-    private Queue<Player> players = new LinkedList<>();
-    private int boardSize = 100;
+// class SnakeLadder {
+//     private Map<Integer, Integer> snakes = new HashMap<>();
+//     private Map<Integer, Integer> ladders = new HashMap<>();
+//     private Queue<Player> players = new LinkedList<>();
+//     private int boardSize = 100;
 
-    public SnakeLadder() {
-        snakes.put(99, 10);
-        snakes.put(70, 55);
+//     public SnakeLadder() {
+//         snakes.put(99, 10);
+//         snakes.put(70, 55);
 
-        ladders.put(3, 22);
-        ladders.put(5, 50);
-    }
+//         ladders.put(3, 22);
+//         ladders.put(5, 50);
+//     }
 
-    public void addPlayer(Player p) {
-        players.add(p);
-    }
+//     public void addPlayer(Player p) {
+//         players.add(p);
+//     }
 
-    private int rollDice() {
-        return new Random().nextInt(6) + 1;
-    }
+//     private int rollDice() {
+//         return new Random().nextInt(6) + 1;
+//     }
 
-    public void startGame() {
-        while (true) {
-            Player current = players.poll();
-            int dice = rollDice();
+//     public void startGame() {
+//         while (true) {
+//             Player current = players.poll();
+//             int dice = rollDice();
 
-            int newPos = current.position + dice;
+//             int newPos = current.position + dice;
 
-            if (snakes.containsKey(newPos)) {
-                newPos = snakes.get(newPos);
-            } else if (ladders.containsKey(newPos)) {
-                newPos = ladders.get(newPos);
-            }
+//             if (snakes.containsKey(newPos)) {
+//                 newPos = snakes.get(newPos);
+//             } else if (ladders.containsKey(newPos)) {
+//                 newPos = ladders.get(newPos);
+//             }
 
-            if (newPos <= boardSize) {
-                current.position = newPos;
-            }
+//             if (newPos <= boardSize) {
+//                 current.position = newPos;
+//             }
 
-            System.out.println(current.name + " -> " + current.position);
+//             System.out.println(current.name + " -> " + current.position);
 
-            if (current.position == boardSize) {
-                System.out.println(current.name + " wins!");
-                break;
-            }
+//             if (current.position == boardSize) {
+//                 System.out.println(current.name + " wins!");
+//                 break;
+//             }
 
-            players.add(current);
-        }
-    }
-}
+//             players.add(current);
+//         }
+//     }
+// }
 
-public class Main {
-    public static void main(String[] args) {
-        SnakeLadder game = new SnakeLadder();
+// public class Main {
+//     public static void main(String[] args) {
+//         SnakeLadder game = new SnakeLadder();
 
-        game.addPlayer(new Player("A"));
-        game.addPlayer(new Player("B"));
+//         game.addPlayer(new Player("A"));
+//         game.addPlayer(new Player("B"));
 
-        game.startGame();
-    }
-}
+//         game.startGame();
+//     }
+// }
