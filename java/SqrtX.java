a525c7a427166e7040558f9635b95bcea1046512
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-04-27 22:57:34 +0530

    Implement binary search for square root calculation

---
+++ java/SqrtX.java
@@ -0,0 +1,14 @@
+class SqrtX {
+    public int mySqrt(int x) {
+        int left = 0, right = x;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid == x) return (int) mid;
+            else if (mid * mid < x) left = (int) mid + 1;
+            else right = (int) mid - 1;
+        }
+        return right;
+    }
+}
