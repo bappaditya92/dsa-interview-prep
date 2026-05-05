e3366f3f087912519a8a51a78c99002103b22a0f
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-05-06 00:09:11 +0530

    Implement minDays method to calculate minimum days

---
+++ java/EatOranges.java
@@ -0,0 +1,16 @@
+class EatOranges {
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int minDays(int n) {
+        if (n <= 1) return n;
+        if (memo.containsKey(n)) return memo.get(n);
+
+        int res = 1 + Math.min(
+                n % 2 + minDays(n / 2),
+                n % 3 + minDays(n / 3)
+        );
+
+        memo.put(n, res);
+        return res;
+    }
+}
