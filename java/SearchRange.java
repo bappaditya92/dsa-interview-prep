d5f0a1ab2a3fac161d74399e8e63d17a847eda66
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-05-10 19:16:23 +0530

    Add SearchRange class for target position search
    Implement binary search to find the first and last positions of a target in a sorted array.

---
+++ java/SearchRange.java
@@ -0,0 +1,53 @@
+class SearchRange {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int first = firstPosition(nums, target);
+        int last = lastPosition(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int firstPosition(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int lastPosition(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1
