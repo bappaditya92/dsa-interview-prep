64e551e34cc3005b9f7ff1a0f1948c6c0180f4a0
bappaditya92 <bappadityasinha.92@gmail.com>
2026-05-18 11:03:43 +0530

    refactor: reorganize project structure for better maintainability

--- java/SearchRotatedII.java
+++ java/SearchRotatedII.java
@@ -1,48 +1,29 @@
 class SearchRotatedII {
-
-    public boolean search(int[] nums,
-                          int target) {
-
-        int left = 0;
-        int right = nums.length - 1;
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
 
         while (left <= right) {
+            int mid = (left + right) / 2;
 
-            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
 
-            if (nums[mid] == target)
-                return true;
-
-            if (nums[left] == nums[mid]
-                    && nums[mid] == nums[right]) {
-
+            // handle duplicates
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
                 left++;
                 right--;
-
-            } else if (nums[left] <= nums[mid]) {
-
-                if (target >= nums[left]
-                        && target < nums[mid]) {
-
+            }
+            else if (nums[left] <= nums[mid]) { // left sorted
+                if (nums[left] <= target && target < nums[mid])
                     right = mid - 1;
-
-                } else {
+                else
                     left = mid + 1;
-                }
-
-            } else {
-
-                if (target > nums[mid]
-                        && target <= nums[right]) {
-
+            } else { // right sorted
+                if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
-
-                } else {
+                else
                     right = mid - 1;
-                }
             }
         }
-
         return false;
     }
 }
