965e83fed0ded823db609f6100d5f835cda41ba5
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-05-23 22:54:21 +0530

    Implement ValidAnagram class with isAnagram method

---
+++ java/ValidAnagram.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            count[s.charAt(i) - 'a']++;
+
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num : count) {
+
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isAnagram("listen", "silent"));
+    }
+}
