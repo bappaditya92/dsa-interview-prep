eea74ae4c1550ae7e117c28530b664c8b6f8d586
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-05-30 23:11:22 +0530

    Add ArrayEqual class to compare two arrays

---
+++ java/ArrayEqual.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArrayEqual {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {1, 2, 3};
+
+        boolean result = Arrays.equals(arr1, arr2);
+
+        System.out.println(result);
+    }
+}
