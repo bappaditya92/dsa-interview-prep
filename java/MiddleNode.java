8c3482870e6bc27a65a0c40affdd05cee291dc0c
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-06-01 22:52:42 +0530

    Implement findMiddle method to locate middle node

---
+++ java/MiddleNode.java
@@ -0,0 +1,26 @@
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class MiddleNode {
+
+    public static Node findMiddle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
