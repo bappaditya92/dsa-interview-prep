8ebba1307856bca98e7c8db738eb8ba1da94cf33
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-06-14 22:26:04 +0530

    Implement TaskSchedular class for task scheduling

---
+++ java/TaskSchedular.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class TaskSchedular {
+
+    public int leastInterval(
+            char[] tasks,
+            int n) {
+
+        int[] freq = new int[26];
+
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        Arrays.sort(freq);
+
+        int maxFreq =
+                freq[25] - 1;
+
+        int idleSlots =
+                maxFreq * n;
+
+        for (int i = 24;
+             i >= 0 && freq[i] > 0;
+             i--) {
+
+            idleSlots -=
+                    Math.min(
+                            freq[i],
+                            maxFreq
+                    );
+        }
+
+        return idleSlots > 0
+                ? tasks.length + idleSlots
+                : tasks.length;
+    }
+}
