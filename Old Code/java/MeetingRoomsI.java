aa74f35b060e0bcedade24ac163388f8521a319e
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-07-06 22:14:51 +0530

    Add MeetingRoomsI class to check meeting conflicts

---
+++ Old Code/java/MeetingRoomsI.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MeetingRoomsI {
+
+    public static boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {
+                {0, 30},
+                {5, 10},
+                {15, 20}
+        };
+
+        System.out.println(canAttendMeetings(intervals));
+    }
+}
