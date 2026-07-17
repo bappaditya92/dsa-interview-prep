81946270425183136f4998cac4e1ca72199e2447
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-07-17 23:36:10 +0530

    Add FrequencyCount class to count element frequencies

---
+++ Old Code/java/FrequencyCount.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,3,3,4};
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        System.out.println(map);
+    }
+}
