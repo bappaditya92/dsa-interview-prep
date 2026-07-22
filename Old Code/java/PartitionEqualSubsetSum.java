a34ca571ac469a6e4dffaea1a79b0f343dab7467
Bappaditya Sinha <90059187+bappaditya92@users.noreply.github.com>
2026-07-22 23:53:03 +0530

    Add PartitionEqualSubsetSum.java for subset partitioning

---
+++ Old Code/java/PartitionEqualSubsetSum.java
@@ -0,0 +1,36 @@
+public class PartitionEqualSubsetSum {
+
+    public static boolean canPartition(int[] nums) {
+
+        int sum = 0;
+
+        for (int num : nums)
+            sum += num;
+
+        // If total sum is odd, partition is impossible
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+
+            // Traverse backwards to avoid reusing the same element
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,5,11,5};
+
+        System.out.println(canPartition(nums)); // true
+    }
+}
